4d3d6b79f2e061d788d28b7ad4bae1e8eab0e5ba
xebec19 <rohant946@gmail.com>
2023-03-07 23:05:56 +0530

    feat: added algolia search box and hit card

--- components/UI/NavigationUI.tsx
+++ components/UI/NavigationUI.tsx
@@ -1,14 +1,25 @@
 import styled from "styled-components";
 import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
+import StorefrontRoundedIcon from "@mui/icons-material/StorefrontRounded";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import Badge from "@mui/material/Badge";
 import classes from "../../styles/ui.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navigation() {
+  const router = useRouter();
+
   return (
     <div className={classes.navBar}>
-      <StorefrontOutlinedIcon />
+      <Link href="/">
+        {router.pathname == "/" ? (
+          <StorefrontRoundedIcon />
+        ) : (
+          <StorefrontOutlinedIcon />
+        )}
+      </Link>
       <Badge badgeContent={4} color="primary">
         <ShoppingCartOutlinedIcon />
       </Badge>
